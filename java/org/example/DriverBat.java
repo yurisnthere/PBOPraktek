710dbc93b542c88bfb65b63ab758ea4f6243c891
grey <132223403+yurisnthere@users.noreply.github.com>
2024-03-04 14:53:47 +0700

    Add files via upload

---
+++ java/org/example/DriverBat.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class DriverBat {
+    public static void main(String[] args) {
+        bat bat1 = new bat();
+        bat1.behavior();
+
+    }
+}
+
---
+++ java/org/example/bat.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class bat {
+     public String name = " bat";
+        public int age = 3;
+       public String gender = "Male";
+       public String food ="Vegetables";
+
+    public void behavior (){
+        System.out.println("Name :"+name);
+        System.out.println("Age : "+ age);
+        System.out.println("Gender :"+ gender);
+        System.out.println("Food :"+ food);
+    }
+
+}
